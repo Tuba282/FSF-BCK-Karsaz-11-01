61c7500cd8efdd7641bc3386093f7451b7cd88f2
Tuba282 <tubajan282@gmail.com>
2026-06-29 13:30:39 +0500

    JS start + variables + datatypes

---
+++ class-08/app.js
@@ -0,0 +1,63 @@
+// console.log("My First Programming Code of JavaScript")
+
+// output
+// testing 
+// debugging
+
+// write a code in javascript to see outoput in console
+
+// console.log("Eman Fatima")
+
+// console.log();
+
+
+//  js -- single line comment ---> //
+//  js -- multi line comment ---> /*---- content ----*/
+
+
+// console.log("Sabeeha");
+// console.log(50);
+
+
+// --- DataTYpes ---
+// String --- characters abc 1234 !@#$%^& " '🚫 "" '' " -- "Ali"
+// Number --- numbers ---unlimitd numbers but do not start with 0
+// Boolean --- yes/no --- true ya false
+
+
+
+
+// String ---
+// console.log("Muhammad Saad is 24 years old .");
+
+
+// number
+// console.log(55);
+
+
+// Bollean
+// console.log(true);
+
+
+
+// To store Data --- Variables -- Containers
+
+
+// let variableName = data 
+
+// let firstBox = "ChatMasala"
+// let secondBox = "Namak";
+// console.log(firstBox);
+
+
+// Write a programm to print Ali Ahmed Name and his age and check weather he is present or not.
+
+let name = "Ali Ahmed"
+let age = 24
+let isPresent = true
+
+
+console.log(name);
+console.log(age)
+console.log(isPresent);
+
