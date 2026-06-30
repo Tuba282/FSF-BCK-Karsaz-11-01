f7df6634b37a4570a05b969779a052e3eaa6ed72
Tuba282 <tubajan282@gmail.com>
2026-06-30 13:23:31 +0500

    comparision operators

---
+++ class-09/app.js
@@ -0,0 +1,54 @@
+// console.log("Hello World");
+// output 
+// debugging
+// testing
+
+
+// alert("Hello World")
+
+
+// Datatypes 
+// String--- acb---z  ""
+// Boolean 
+// Numbers
+
+// Variables 
+
+
+
+let VendorName = 'Ali Zafar'
+let stock = 12
+let isPinkBagAvailable = true
+
+// console.log(VendorName);
+// console.log(stock);
+// console.log(isPinkBagAvailable);
+
+// console.log("Abdul Majeed Has 25 bags in his shop and also has Pink one <-- this statement is false.");
+
+// // console.log(``);  templete litterals
+
+// console.log(`${VendorName} Has ${stock} bags in his shop and also has Pink one <-- this statement is ${isPinkBagAvailable}.`);
+
+
+// -- Comparision Operators
+
+
+// Comparision Operators > < >= <= == === != !== 
+// always return  boolean -- true / false
+
+
+// console.log(10 > 10);//false
+// console.log(10 >= 10);//true
+
+
+// console.log("123" === 123);//true 
+// console.log(5 != 5);//false
+
+
+// console.log(7 !== 9);//true
+
+
+// --- Conditions ---------------
+
+
